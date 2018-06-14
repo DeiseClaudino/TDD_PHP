7eba7c5d69cf8c178b7d20a71b324e2e3fd58e5e
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-06-14 15:29:40 -0300

    removendo  Teste de matemática maluca

--- Avaliador.php
+++ Avaliador.php
@@ -54,11 +54,3 @@ class Avaliador
 
 
 }
-
-class MatematicaMaluca {
-public function contaMaluca($numero) {
-    if($numero > 30) return $numero * 4;
-    else if($numero > 10) return $numero * 3;
-    else return $numero * 2;
-}
-}

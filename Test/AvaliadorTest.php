3f619a5d24433838aca07096e5c2d3c97d28a7a3
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-06-15 15:07:09 -0300

    Criando teste que verifica leilao com um  lance e criação do setUp, os testes não funcionaram

--- Test/AvaliadorTest.php
+++ Test/AvaliadorTest.php
@@ -5,44 +5,54 @@ require_once 'carregaClasses.php';
 
 class AvaliadorTest extends PHPUnit\Framework\TestCase
 {
+    private $leiloeiro;
+    private $joao;
+    private $renan;
+    private $felipe;
+
+    public function setUp()
+    {
+        $this->leiloeiro = new Avaliador();
+        $this->joao = new Usuario("Joao");
+        $this->renan = new Usuario("Renan");
+        $this->felipe = new Usuario("Felipe");
+    }
+    
     public function testEmOrdemDecrescente()
     {
         $leilao = new Leilao("Playstation 3");
 
-        $joao = new Usuario("Joao");
-        $renan = new Usuario("Renan");
-        $felipe = new Usuario("Felipe");
+        $leilao->propoe(new Lance($this->joao, 300));
+        $leilao->propoe(new Lance($this->renan, 400));
+        $leilao->propoe(new Lance($this->felipe, 250));
 
-        $leilao->propoe(new Lance($joao, 300));
-        $leilao->propoe(new Lance($renan, 400));
-        $leilao->propoe(new Lance($felipe, 250));
-
-        $leiloeiro = new Avaliador();
         $leiloeiro->avalia($leilao);
-
-        $maiorEsperado = 400;
-        $menorEsperado = 250;
 
         $this->assertEquals($maiorEsperado, $leiloeiro->getMaiorLance());
         $this->assertEquals($menorEsperado, $leiloeiro->getMenorLance());
     }
+
+    public function testDeveEntenderLeilaoComApenasUmLance()
+    {
+        $leilao = new Leilao("Playstation 3 Novo");
+
+        $leilao->propoe(new Lance($joao, 1000.0));
+
+        $leiloeiro->avalia($leilao);
+
+        $this->assertEquals(1000.0, $leiloeiro->getMaiorLance(), 0.00001);
+        $this->assertEquals(1000.0, $leiloeiro->getMenorLance(), 0.00001);
+    }
+
 
 
     public function testpegaOstresLMaiores()
     {
         $leilao = new Leilao("Playstation 3");
 
-        $joao = new Usuario("Joao");
-        $renan = new Usuario("Renan");
-        $felipe = new Usuario("Felipe");
-
-
-
         $leilao->propoe(new Lance($joao, 250));
         $leilao->propoe(new Lance($renan, 300));
         $leilao->propoe(new Lance($felipe, 400));
-
-        $leiloeiro = new Avaliador();
 
         $leiloeiro->avalia($leilao);
 
@@ -54,7 +64,4 @@ class AvaliadorTest extends PHPUnit\Framework\TestCase
         $this->assertEquals($maiores[1]->getValor(), 300);
         $this->assertEquals($maiores[2]->getValor(), 250);
     }
-
-
-
 }
